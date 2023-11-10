0ac223accf910e74e95879ce893b860ba533aed2
Juan Yannuzzi <jyannuzzi@correo.um.edu.uy>
2023-11-10 00:21:53 -0300

    estructura Race y racing_team

---
+++ entities/racing_team.py
@@ -0,0 +1,34 @@
+import datetime
+import Race
+class Racing_Team(Race):
+    def __init__(self):
+        self._team_name= ""
+        self._nationality = ""
+        self.team_fundation_date = datetime.datetime.now()
+        self.team_car_model = ""
+        self.team_score = ""
+        self.members = int
+        self._racers = []
+        self._mechanics = []
+        self._directors = []
+        self._official_racers = []
+
+    @property
+    def get_team_name(self):
+        return self._team_name
+    
+    @property
+    def get_team_car_model(self):
+        return self.team_car_model
+    
+    @property
+    def get_members(self):
+        return self.members
+    
+    @property
+    def get_racers(self):
+        return self._racers
+    
+    @property
+    def get_official_racers(self):
+        return self._official_racers
