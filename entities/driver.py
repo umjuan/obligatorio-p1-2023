ca1f7e5a04dfc99ee887691303e1f11599add52c
JUAN-ANDRES-ROBALLO <juanroballo85@gmail.com>
2023-11-13 20:33:48 -0300

    changes in visibility

--- entities/driver.py
+++ entities/driver.py
@@ -3,26 +3,26 @@ from employee import Employee
 class Driver(Employee):
     def __init__(self,id,name,age,nationality,born,salary,score,total_score,car_number,injury=False):
         Employee.__init__(self,id,name,age,nationality,born,salary)
-        self.__score=score
-        self.__total_score=total_score
-        self.__car_number=car_number
-        self.__injury=injury
+        self._score=score
+        self._total_score=total_score
+        self._car_number=car_number
+        self._injury=injury
 
     @property
     def score(self):
-        return  self.__score
+        return  self._score
     @property
     def car_number(self):
-        return  self.__car_number
+        return  self._car_number
     @property
     def total_score(self):
-        return self.__total_score
+        return self._total_score
     @total_score.setter
     def total_score(self,other):
-        self.__total_score=other
+        self._total_score=other
     @property
     def injury(self):
-        return self.__injury
+        return self._injury
     @injury.setter
     def injury(self,other):
-        self.__injury=other
+        self._injury=other
