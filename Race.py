c9c57f60fe43fa8c458428ed63ddfcdc1f399b22
JUAN-ANDRES-ROBALLO <juanroballo85@gmail.com>
2023-11-17 18:42:06 -0300

    final

--- Race.py
+++ Race.py
@@ -8,21 +8,7 @@ from exceptions.exceptions import *
 from datetime import datetime
 
 class Race(object):
-    employees = Employee
-    drivers = Driver
-    directors = Director
-    cars = Car
     
-
-    def __init__(self):
-        teams = Team
-        employees = Employee
-        drivers = Driver
-        directors = Director
-        self._racers = []
-        self._teams = []
-        self._results = []
-
     def regist_unexpected(self):
         self.param = list(input('Insert comma separated racers number which left the race. '))
         self.param2 = list(input('Insert racer numbers who reported errors in pits. '))
@@ -85,12 +71,12 @@ class Race(object):
             listteam.append(i.team_name)
             for x in listpivot1:
                 if x in i.drivers:
-                    x.append(listteam)
+                    listteam.append(x)
             listcomp.append(listteam)    
         return listcomp
 
     
-    def get_penalties(self):
+    def get_penalties(self): 
         listcomp=[]
         listpivot1=[]
         listpivot=self.param3
@@ -104,7 +90,7 @@ class Race(object):
             listteam.append(i.team_name)
             for x in listpivot1:
                 if x in i.drivers:
-                    x.append(listteam)
+                    listteam.append(x)
             listcomp.append(listteam)    
         return listcomp
 
@@ -122,20 +108,53 @@ class Race(object):
         return self._results
     
     def simulate_race(self):
-        self.regist_unexpected
-        teams = Team
-        employees = Employee
-        drivers = Driver
-        directors = Director
-        cars = Car.cars
-        if self.get_left_race in cars:
-            self.cars = self.cars - self.get_param
-        if self.get_pits_error in cars:
-            self.cars = self.cars - self.get_pits_error
-        if self.get_penalties in cars:
-            self.cars = self.cars - self.get_penalties
-        for faults in self.cars:
-            cars.append(faults)
+        self.regist_unexpected()
+        part=self.get_left_race()
+        listrace=[]
+        for i in part:
+            listdriver=[]
+            if len(i)!=1:
+                for x in i[1:]:
+                    lista=[]
+                    suma1=0
+                    for z in Team.team_list:
+                        if x in z.drivers:
+                            num=[n for n in z.mechanics]
+                            num1=int(z.car_model.score)
+                            num3=z.team_leader
+                            break
+                    for y in Employee.employee_list:
+                        if x==y.id:
+                            num2=int(y.score)
+                        if num3==y.id:
+                            num4=int(y.score)
+                        for w in num:
+                            if w==y.id:
+                                suma1+=int(y.score)
+                                break
+                    sumafinal=num1+num2+num4+suma1
+                    lista.append(x)
+                    lista.append(i[0])
+                    lista.append(sumafinal)
+                    listdriver.append(lista)       
+            listrace.append(listdriver)
+        part1=self.get_pits_error()
+        for i in part1:
+            for x in range(len(listrace)):
+                num8=part1.count(listrace[x][0])
+                listrace[x][2]-=num8*5
+        part2=self.get_penalties()
+        for i in part1:
+            for x in range(len(listrace)):
+                num8=part1.count(listrace[x][0])
+                listrace[x][2]-=num8*8
+        for i in listrace:
+            print(i)
+                
+                
+
+
+
 
     def query_top10_chmp(self):
         #Perform query
