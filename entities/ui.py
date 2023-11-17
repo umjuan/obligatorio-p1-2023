9fe7c01767c96d5dff6a8dab79ecf41faa788700
JUAN-ANDRES-ROBALLO <juanroballo85@gmail.com>
2023-11-16 21:31:17 -0300

    changes

--- entities/ui.py
+++ entities/ui.py
@@ -1,4 +1,25 @@
 check1=True
 while check1:
-    print('1 Alta de empleado','2 Alta de auto','3 Alta de equipo','4 Simular carrera','5 Realizar consultas','6 Finalizar programa',sep=' ')  
+    check2=True
+    print('1 Alta de empleado','2 Alta de auto','3 Alta de equipo','4 Simular carrera','5 Realizar consultas','6 Finalizar programa',sep='\n')  
     ask1=input('ELIGA LA OPCION: ')     
+    if ask1=='1':
+        emp1=input('INGRESE CEDULA: ')
+        emp2=input('INGRESE NOMBRE: ')
+        emp3=input('INGRESE FECHA DE NACIMIENTO: ')
+        emp4=input('INGRESE NACIONALIDAD: ')
+        emp5=input('INGRESE SALARIO:' )
+        print('CARGOS: ','1 piloto','2 piloto de reserva','3 mecanico','4 jefe de equipo',sep='\n')
+        emp6=input('INGRESE LA OPCION: ')
+        if emp6=='1':
+            
+    if ask1=='2':
+        pass
+    if ask1=='3':
+        pass
+    if ask1=='4':
+        pass
+    if ask1=='5':
+        pass
+    if ask1=='6':
+        pass
